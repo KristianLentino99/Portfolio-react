a4dea358094d0ae6fe20be14f4f4f9fe8818322c
Kristian Lentino <kristian.lentino@dieffetech.it>
2021-12-24 16:08:29 +0100

    miglioramenti grafici

--- src/components/wave/WaveComponent.tsx
+++ src/components/wave/WaveComponent.tsx
@@ -7,11 +7,18 @@ function WaveComponent() {
         setWaveWidth(100);
     },[]);
     return (
-        <div className="wave" style={{width:`${waveWidth}%`}}>
-            <svg viewBox="0 0 500 500" preserveAspectRatio="xMinYMin meet">
-                <path d="M0,100 C150,200 350,0 500,100 L500,00 L0,0 Z" style={{stroke:"none",fill:"var(--primary-color)"}}></path>
-            </svg>
-        </div>
+        <React.Fragment>
+            <div className="wave" style={{width:`${waveWidth}%`}}>
+                <svg viewBox="0 0 500 500" preserveAspectRatio="xMinYMin meet">
+                    <path d="M0,100 C150,200 350,0 500,100 L500,00 L0,0 Z" style={{stroke:"none",fill:"var(--primary-color)"}}></path>
+                </svg>
+            </div>
+            <div className="wave-black" style={{width:`${waveWidth - 18}%`}}>
+                <svg viewBox="0 0 500 500" preserveAspectRatio="xMinYMin meet">
+                    <path d="M0,100 C150,200 350,0 500,100 L500,00 L0,0 Z" style={{stroke:"none",fill:"black"}}></path>
+                </svg>
+            </div>
+        </React.Fragment>
     );
 }
 export default WaveComponent;
